2a7e6c93095a042e61fc4d3d2b385bab6c9cb1de
yusuke.nagao <y.n.227@icloud.com>
2021-10-14 12:28:20 +0000

    CRUD messages

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,59 @@
+class TasksController < ApplicationController
+    def index
+        @tasks = Task.all
+    end
+    
+    def show
+        @tasks = Task.find(params[:id])
+    end
+    
+    def new
+        @tasks = Task.new
+    end
+    
+    def create
+        @tasks = Task.new(tasks_params)
+
+        if @tasks.save
+            flash[:success] = 'Task が正常に投稿されました'
+            redirect_to @tasks
+    
+        else
+            flash.now[:danger] = 'Task が投稿されませんでした'
+            render :new
+        end
+    end
+    
+    def edit
+        @tasks = Task.find(params[:id])
+    end
+    
+    def update
+        @tasks = Task.find(params[:id])
+        
+        if @task.update(tasks_params)
+            flash[:success] = 'Task は正常に更新されました'
+            redirect_to @tasks
+            
+        else
+            flash.now[:danger] = 'Task は更新されませんでした'
+            render :edit
+        end
+        
+    end
+    
+    def destroy
+        @tasks = Task.find(params[:id])
+        @tasks.destroy
+        
+        flash[:success] = 'Task は正常に削除されました'
+        redirect_to tasks_url
+    end
+    
+end
+
+private
+
+  def tasks_params
+    params.require(:tasks).permit(:content)
+  end
