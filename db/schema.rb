01ba2152ecfbf5677d4f3408a99b6161a3247b2d
yusuke.nagao <y.n.227@icloud.com>
2021-10-15 06:13:46 +0000

    kadai_syuusei2

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_10_14_134547) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "status"
+    t.string "status"
   end
 
 end
